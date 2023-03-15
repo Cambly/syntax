5cbbd51fdcdcbe39401a7d842df6fe2181f24dc8
Joe Liotta <joe.liotta00@gmail.com>
2023-03-15 12:43:32 -0700

    Address pr comments

--- packages/syntax-core/src/Card/Card.stories.tsx
+++ packages/syntax-core/src/Card/Card.stories.tsx
@@ -10,12 +10,6 @@ export default {
       url: "https://www.figma.com/file/p7LKna9JMU0JEkcKamzs53/%F0%9F%93%90-Syntax?node-id=1206-4420&t=yFh7Ijhf6PU7Lin3-0",
     },
   },
-  argTypes: {
-    size: {
-      options: ["sm", "md", "lg", "xl"],
-      control: { type: "radio" },
-    },
-  },
   tags: ["autodocs"],
 } as Meta<typeof Card>;
 
@@ -26,7 +20,7 @@ export const Default: StoryObj<typeof Card> = {
         <h1>Headline</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
       </div>
     ),
