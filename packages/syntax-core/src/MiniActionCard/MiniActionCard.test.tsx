797ca65493093d6213a00e56c3d7c9762954a2e4
Joe Liotta <joe.liotta00@gmail.com>
2023-03-17 20:16:02 -0400

    Add test

---
+++ packages/syntax-core/src/MiniActionCard/MiniActionCard.test.tsx
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import MiniActionCard from "./MiniActionCard";
+
+describe("miniActionCard", () => {
+  it("should render children successfully", () => {
+    const screen = render(
+      <MiniActionCard>
+        <h1>title</h1>
+        <p>text</p>
+      </MiniActionCard>,
+    );
+    expect(screen.getByText(/title/)).toBeInTheDocument();
+    expect(screen.getByText(/text/)).toBeInTheDocument();
+  });
+});
--- packages/syntax-core/src/MiniActionCard/MiniActionCard.tsx
+++ packages/syntax-core/src/MiniActionCard/MiniActionCard.tsx
@@ -1,13 +1,13 @@
 import styles from "./MiniActionCard.module.css";
 
 /**
- * Card is a basic container component to apply consistent styling and render child components.
+ * MiniActionCard is component that alerts users to a call to action.
  */
 const MiniActionCard = ({
   children,
 }: {
   /**
-   * The child components to render within Card.
+   * The child components to render within MiniActionCard.
    */
   children: JSX.Element;
 }): JSX.Element => <div className={styles.miniActionCard}>{children}</div>;
