d28452dc6769f68198ac4a2aa7126a04088ea085
Nate Ong <nateong@nates-mbp.lan>
2023-03-29 14:49:13 -0700

    Update styling. Add comments

--- packages/syntax-core/src/SelectList/SelectList.stories.tsx
+++ packages/syntax-core/src/SelectList/SelectList.stories.tsx
@@ -1,5 +1,6 @@
 import { StoryObj, Meta } from "@storybook/react";
 import SelectList from "./SelectList";
+import React, { useState, ReactElement } from "react";
 
 export default {
   title: "SelectList",
@@ -11,16 +12,54 @@ export default {
       url: "https://www.figma.com/file/p7LKna9JMU0JEkcKamzs53/%F0%9F%93%90-Syntax?node-id=1007-4102&t=7xGt6S9b6zUUiflR-0",
     },
   },
+  argTypes: {
+    size: {
+      options: ["sm", "md", "lg"],
+      control: { type: "radio" },
+    },
+    disabled: {
+      control: "boolean",
+    },
+    error: {
+      control: "boolean",
+    },
+    onChange: { action: "clicked" },
+  },
   tags: ["autodocs"],
 } as Meta<typeof SelectList>;
 
+const options = [
+  { label: "option 1", value: "opt1" },
+  { label: "option 2", value: "opt2" },
+  { label: "option 3", value: "opt3" },
+];
+
 export const Default: StoryObj<typeof SelectList> = {
   args: {
-    options: [
-      { label: "option 1", value: "opt1" },
-      { label: "option 2", value: "opt2" },
-      { label: "option 3", value: "opt3" },
-    ],
-    placeholderText: "Choose one",
+    options,
+    placeholderText: "Placeholder",
+    helperText: "Helper text",
+    label: "Label",
   },
+};
+
+const SelectListInteractive = (): ReactElement => {
+  const [selection, setSelection] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelection(e.target.value);
+  };
+  return (
+    <SelectList
+      label="Label"
+      helperText="Helper text"
+      selectedValue={selection}
+      placeholderText="Placeholder"
+      onChange={onChange}
+      options={options}
+    />
+  );
+};
+
+export const Interactive: StoryObj<typeof SelectList> = {
+  render: () => <SelectListInteractive />,
 };
--- packages/syntax-core/src/SelectList/SelectList.tsx
+++ packages/syntax-core/src/SelectList/SelectList.tsx
@@ -3,7 +3,7 @@ import styles from "./SelectList.module.css";
 import classNames from "classnames";
 import Typography from "../Typography/Typography";
 
-interface OptionData {
+export interface OptionData {
   label: string;
   value: string;
 }
@@ -15,52 +15,137 @@ const selectBoxSize = {
 } as const;
 
 const fontSize = {
-  sm: styles.smFont,
-  md: styles.mdFont,
-  lg: styles.lgFont,
+  sm: 100,
+  md: 200,
+  lg: 300,
 } as const;
+
+const iconSize = {
+  sm: 20,
+  md: 24,
+  lg: 24,
+} as const;
+
+const fontColor = (error: boolean, selected: string) => {
+  if (error) {
+    return "destructive-primary";
+  } else if (selected) {
+    return "gray800";
+  }
+  return "gray700";
+};
 
 const SelectList = ({
   placeholderText,
   options,
   size = "md",
-  unselected = true,
   label,
   helperText,
+  selectedValue = "",
+  onChange,
+  error = false,
 }: {
+  /**
+   * Text showing in select box if no option has been chosen
+   *
+   */
   placeholderText: string;
+  /**
+   * Array of objects {label: string, value: string}
+   *
+   */
   options: OptionData[];
+  /**
+   * Size of the select box
+   * * `sm`: 32px
+   * * `md`: 40px
+   * * `lg`: 48px
+   *
+   * @defaultValue "md"
+   */
   size?: "sm" | "md" | "lg";
-  unselected?: boolean;
+  /**
+   * Text shown above select box
+   */
   label?: string;
+  /**
+   * Text shown below select box
+   */
   helperText?: string;
+  /**
+   * Value of the currently selected option
+   */
+  selectedValue: string;
+  /**
+   * The callback to be called when an option is selected
+   */
+  onChange: React.ChangeEventHandler<HTMLSelectElement>;
+  error?: boolean;
 }): ReactElement => {
-  const selectBox = classNames(
-    styles.selectBox,
-    selectBoxSize[size],
-    fontSize[size],
-    {
-      [styles.placeholderSelection]: unselected,
-    },
+  const divSelectBox = classNames(styles.divSelect, selectBoxSize[size], {
+    [styles.selectError]: error,
+  });
+  const valueToLabel: Record<string, string> = options.reduce(
+    (result, curr) => ({ ...result, [curr.value]: curr.label }),
+    {},
   );
   return (
     <div className={styles.selectContainer}>
-      <Typography size={100} color="gray700">
-        {label}
-      </Typography>
-      <select required className={selectBox}>
-        <option disabled selected value="">
-          {placeholderText}
-        </option>
-        {options.map((o) => (
-          <option key={o.value} value={o.value}>
-            {o.label}
+      <div className={styles.outerTextContainer}>
+        <Typography
+          size={100}
+          color={error ? "destructive-primary" : "gray700"}
+        >
+          {label}
+        </Typography>
+      </div>
+      <div className={styles.selectWrapper}>
+        <select
+          required
+          className={classNames(styles.selectBox, selectBoxSize[size])}
+          onChange={onChange}
+        >
+          <option disabled selected={selectedValue === ""} value="">
+            {placeholderText}
           </option>
-        ))}
-      </select>
-      <Typography size={100} color="gray700">
-        {helperText}
-      </Typography>
+          {options.map((o) => (
+            <option
+              key={o.value}
+              value={o.value}
+              selected={selectedValue === o.value}
+            >
+              {o.label}
+            </option>
+          ))}
+        </select>
+        <div className={divSelectBox}>
+          <Typography
+            size={fontSize[size]}
+            color={fontColor(error, selectedValue)}
+          >
+            {valueToLabel[selectedValue] || placeholderText}
+          </Typography>
+          <svg
+            focusable="false"
+            aria-hidden="true"
+            viewBox="0 0 24 24"
+            width={iconSize[size]}
+          >
+            <path
+              fill={error ? "#d32a4b" : "#353535"}
+              d="M15.88 9.29 12 13.17 8.12 9.29a.9959.9959 0 0 0-1.41 0c-.39.39-.39 1.02 0 1.41l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59c.39-.39.39-1.02 0-1.41-.39-.38-1.03-.39-1.42 0z"
+            ></path>
+          </svg>
+        </div>
+      </div>
+      <div className={styles.outerTextContainer}>
+        <Typography
+          size={100}
+          color={error ? "destructive-primary" : "gray700"}
+        >
+          {helperText}
+        </Typography>
+      </div>
     </div>
   );
 };
