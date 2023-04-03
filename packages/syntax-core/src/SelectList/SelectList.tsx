273d03bb26334238f1f439f5c71ce0fe58ce60d8
Nate Ong <nateong@Nates-MBP.localdomain>
2023-04-03 14:00:48 -0700

    Close path tag

--- packages/syntax-core/src/SelectList/SelectList.tsx
+++ packages/syntax-core/src/SelectList/SelectList.tsx
@@ -71,7 +71,6 @@ const SelectList = ({
   size?: "sm" | "md" | "lg";
 }): ReactElement => {
   const id = useId();
-
   return (
     <div
       className={classNames(styles.selectContainer, {
@@ -120,7 +119,7 @@ const SelectList = ({
             <path
               fill={errorText ? ColorBaseDestructive700 : ColorBaseGray800}
               d="M15.88 9.29 12 13.17 8.12 9.29a.9959.9959 0 0 0-1.41 0c-.39.39-.39 1.02 0 1.41l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59c.39-.39.39-1.02 0-1.41-.39-.38-1.03-.39-1.42 0z"
-            ></path>
+            />
           </svg>
         </div>
       </div>
