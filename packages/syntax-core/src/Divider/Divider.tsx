23bad4be403bc6467115e2595383d2086a655d94
Christian Vuerings <christian@cambly.com>
2023-04-20 05:54:06 -0700

    Divider: allow key to be set as a prop

--- packages/syntax-core/src/Divider/Divider.tsx
+++ packages/syntax-core/src/Divider/Divider.tsx
@@ -3,7 +3,7 @@ import styles from "./Divider.module.css";
 /**
  * Divider is a thin horizontal line to group content in lists and layouts.
  */
-export default function Divider({}: Record<string, never>) {
+export default function Divider(): React.ReactElement {
   return <hr className={styles.divider} />;
 }
 
