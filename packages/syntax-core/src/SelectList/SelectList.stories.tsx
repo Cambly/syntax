6d87913664280aee22c147f4c598680f126747ec
Nate Ong <nateong@Nates-MBP.localdomain>
2023-04-20 10:33:20 -0700

    Update story for visiblity selection

--- packages/syntax-core/src/SelectList/SelectList.stories.tsx
+++ packages/syntax-core/src/SelectList/SelectList.stories.tsx
@@ -21,6 +21,10 @@ export default {
     disabled: {
       control: "boolean",
     },
+    labelDisplay: {
+      options: ["visible", "hidden"],
+      control: { type: "radio" },
+    },
     onChange: { action: "clicked" },
   },
   tags: ["autodocs"],
