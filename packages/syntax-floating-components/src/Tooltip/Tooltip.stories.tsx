31ef940e3525a5beb6e498e4d2bf52864c7f087c
Jeffrey Choe <jeffrey@cambly.com>
2023-04-27 13:47:05 -0700

    storybook changes

--- packages/syntax-floating-components/src/Tooltip/Tooltip.stories.tsx
+++ packages/syntax-floating-components/src/Tooltip/Tooltip.stories.tsx
@@ -1,7 +1,8 @@
+import { useState, useRef } from "react";
 import { StoryObj, Meta } from "@storybook/react";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./Tooltip";
 import styles from "./Tooltip.module.css";
-import { useState, useRef } from "react";
+import type { Placement } from "@floating-ui/react";
 import image from "../../../../apps/storybook/assets/images/book.svg";
 
 export default {
@@ -14,31 +15,52 @@ export default {
     },
   },
   argTypes: {
-    string: {
-      control: "text",
+    children: {
+      control: { type: null },
+    },
+    tooltipText: {
+      control: { type: "text" },
+      table: {
+        defaultValue: { summary: "This is a book" },
+      },
+    },
+    placement: {
+      control: { type: "select" },
+      defaultValue: "top",
+      options: [
+        "top",
+        "top-start",
+        "top-end",
+        "right",
+        "right-start",
+        "right-end",
+        "bottom",
+        "bottom-start",
+        "bottom-end",
+        "left",
+        "left-start",
+        "left-end",
+      ],
+      table: {
+        defaultValue: { summary: "top" },
+      },
+    },
+    initialOpen: {
+      control: { type: null },
+    },
+    open: {
+      control: { type: null },
     },
   },
   tags: ["autodocs"],
 } as Meta<any>;
 
-export const Default: StoryObj<typeof Tooltip> = {
-  render: () => (
-    <>
-      <h1>Floating UI — Tooltip (uncontrolled)</h1>
-      <Tooltip>
-        <TooltipTrigger>My trigger</TooltipTrigger>
-        <TooltipContent className={styles.tooltip}>My tooltip</TooltipContent>
-      </Tooltip>
-    </>
-  ),
-};
-
-export const ControlledTooltip = () => {
-  const [open, setOpen] = useState(false);
+const UnControlledTooltip = ({ args }: any) => {
   const imgRef = useRef(null);
   return (
-    <Tooltip open={open} onOpenChange={setOpen} placement="right">
-      <TooltipTrigger asChild onClick={() => setOpen((v) => !v)}>
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    <Tooltip placement={args.placement as Placement}>
+      <TooltipTrigger asChild>
         <img
           ref={imgRef}
           src={image as string}
@@ -46,7 +68,28 @@ export const ControlledTooltip = () => {
           style={{ width: "50px", height: "50px" }}
         />
       </TooltipTrigger>
-      <TooltipContent className={styles.tooltip}>My tooltip</TooltipContent>
+      <TooltipContent className={styles.tooltip}>
+        {/*eslint-disable-next-line @typescript-eslint/no-unsafe-member-access*/}
+        {args.tooltipText || "This is a book"}
+      </TooltipContent>
+    </Tooltip>
+  );
+};
+
+export const Default: StoryObj<typeof Tooltip> = {
+  render: (args) => <UnControlledTooltip args={{ ...args }} />,
+};
+
+export const ControlledTooltip = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <Tooltip open={open} onOpenChange={setOpen} placement="bottom">
+      <TooltipTrigger onClick={() => setOpen((v) => !v)}>
+        My trigger
+      </TooltipTrigger>
+      <TooltipContent className={styles.tooltip}>
+        This is a tooltip
+      </TooltipContent>
     </Tooltip>
   );
 };
