b698d2d81c6cc78f267732b787b8ee1805b3088f
Jeffrey Choe <jeffrey@cambly.com>
2023-08-30 15:40:59 -0700

    just take in the types we expect

--- packages/syntax-core/src/RadioButton/RadioButton.tsx
+++ packages/syntax-core/src/RadioButton/RadioButton.tsx
@@ -71,7 +71,7 @@ const RadioButton = ({
   /**
    * Value of the selected radio option
    */
-  value?: string | readonly string[] | number | undefined;
+  value?: string | number;
 }): ReactElement => {
   const [isFocused, setIsFocused] = useState(false);
   const { isFocusVisible } = useFocusVisible();
