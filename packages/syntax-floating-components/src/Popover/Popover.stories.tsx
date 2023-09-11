692c3ea7a7a0b867a92b2550fec72536f49e9e80
benjroy <ben.allen@cambly.com>
2023-09-10 21:08:24 -0400

    WIP: Popover v2

--- packages/syntax-floating-components/src/Popover/Popover.stories.tsx
+++ packages/syntax-floating-components/src/Popover/Popover.stories.tsx
@@ -1,13 +1,29 @@
 import { useState, useRef, useEffect, type ReactElement } from "react";
 import type { StoryObj, Meta } from "@storybook/react";
 import { Box, Typography, Button } from "../../../syntax-core/src/index";
-import { Popover, PopoverContent, PopoverTrigger } from "./Popover";
+import Popover from "./Popover";
 import IconButton from "../../../syntax-core/src/IconButton/IconButton";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import Tooltip from "../Tooltip/Tooltip";
 
 export default {
   title: "Floating-Components/Popover",
   component: Popover,
+  decorators: [
+    (Story) => (
+      <Box
+        width="calc(100vw - 2rem)"
+        height="calc(100vh - 2rem)"
+        backgroundColor="gray100"
+        display="flex"
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Story />
+      </Box>
+    ),
+  ],
   parameters: {
     design: {
       type: "figma",
@@ -46,6 +62,7 @@ export default {
       },
     },
     children: {},
+    content: {},
     maxWidth: {
       control: { type: "number" },
     },
@@ -55,11 +72,18 @@ export default {
 
 export const Default: StoryObj<typeof Popover> = {
   args: {
-    delay: 0,
     placement: "bottom",
     initialOpen: true,
-    strategy: "absolute",
     children: (
+      <IconButton
+        accessibilityLabel="Info Icon Button"
+        icon={InfoOutlinedIcon}
+        onClick={() => alert("Default button pressed")}
+        color="tertiary"
+        size="lg"
+      />
+    ),
+    content: (
       <Box width="100%" display="flex" direction="column" gap={2}>
         <Typography transform="uppercase" color="gray700" size={100}>
           Weekly Plan
@@ -72,26 +96,14 @@ export const Default: StoryObj<typeof Popover> = {
       </Box>
     ),
   },
-  render: ({ delay, placement, initialOpen, strategy, children }) => (
+  render: ({ placement, initialOpen, content, children }) => (
     <div style={{ margin: "240px" }}>
       <Popover
-        delay={delay}
         placement={placement}
         initialOpen={initialOpen}
-        strategy={strategy}
+        content={<Box maxWidth="400px" width="100%">{content}</Box>}
       >
-        <PopoverTrigger>
-          <IconButton
-            accessibilityLabel="Info Icon Button"
-            icon={InfoOutlinedIcon}
-            onClick={() => alert("Default button pressed")}
-            color="tertiary"
-            size="lg"
-          />
-        </PopoverTrigger>
-        <PopoverContent maxWidth="400px" width="100%">
-          {children}
-        </PopoverContent>
+        {children}
       </Popover>
     </div>
   ),
@@ -100,7 +112,6 @@ export const Default: StoryObj<typeof Popover> = {
 export const ControlledPopover = (): ReactElement => {
   const [open, setOpen] = useState(false);
   const scrollableRef = useRef<HTMLDivElement>(null);
-  const ref = useRef(null);
 
   useEffect(() => {
     if (scrollableRef.current) {
@@ -111,47 +122,70 @@ export const ControlledPopover = (): ReactElement => {
   }, [scrollableRef]);
 
   return (
-    <div
-      ref={scrollableRef}
-      style={{
-        overflowY: "auto",
-        width: "400px",
-        height: "300px",
-      }}
-    >
-      <Box
-        marginTop={12}
-        marginBottom={12}
-        display="flex"
-        justifyContent="center"
+    <>
+      <div
+        ref={scrollableRef}
+        style={{
+          overflowY: "auto",
+          width: "400px",
+          height: "300px",
+        }}
       >
-        <Popover
-          open={open}
-          placement="bottom"
-          onOpen={() => setOpen(true)}
-          onClose={() => setOpen(false)}
+        <Box
+          marginTop={12}
+          marginBottom={12}
+          display="flex"
+          justifyContent="center"
         >
-          <PopoverTrigger>
-            <Button
-              ref={ref}
-              text="My trigger"
-              onClick={() => setOpen((v) => !v)}
-            />
-          </PopoverTrigger>
-          <PopoverContent maxWidth="700px" width="100%">
-            <Box width="100%" display="flex" direction="column" gap={2}>
-              <Typography transform="uppercase" color="gray700" size={100}>
-                Weekly Plan
-              </Typography>
-              <Typography weight="bold" size={500}>
-                30 Minutes x 2
-              </Typography>
-              <Typography>Daily maximum of 30 minutes</Typography>
-              <Button text="Change Plan" color="secondary" fullWidth />
-            </Box>
-          </PopoverContent>
-        </Popover>
-      </Box>
-    </div>
+          <Popover
+            open={open}
+            placement="bottom"
+            onChangeContentVisibility={(visible) => setOpen(visible)}
+            content={(
+              <Box maxWidth="700px" width="100%">
+                <Box width="100%" display="flex" direction="column" gap={2}>
+                  <Typography transform="uppercase" color="gray700" size={100}>
+                    Weekly Plan
+                  </Typography>
+                  <Typography weight="bold" size={500}>
+                    30 Minutes x 2
+                  </Typography>
+                  <Typography>Daily maximum of 30 minutes</Typography>
+                  <Button text="Change Plan" color="secondary" fullWidth />
+                </Box>
+              </Box>
+            )}
+          >
+            <Button text="Internal trigger" />
+          </Popover>
+        </Box>
+      </div >
+      <Button
+        text="External trigger"
+        onClick={() => setOpen((v) => !v)}
+      />
+    </>
   );
 };
+
+export const ContentHasTooltips = ({ placement }): ReactElement => (
+  <Popover
+    placement={placement}
+    initialOpen
+    content={(
+      <Box width="100%" maxWidth="400px" display="flex" direction="column" gap={2}>
+        <Typography transform="uppercase" color="gray700" size={100}>
+          Weekly Plan <Tooltip content="Save more by subscribing to our monthly or yearly plans!"><InfoOutlinedIcon fontSize="inherit" /></Tooltip>
+        </Typography>
+        <Typography weight="bold" size={500}>
+          30 Minutes x 2 <Tooltip content="One 30 minute or two 15 minute lessons, twice per week"><InfoOutlinedIcon fontSize="inherit" /></Tooltip>
+        </Typography>
+        <Typography tooltip="This is a tooltip">Daily maximum of 30 minutes</Typography>
+        <Button text="Change Plan" color="secondary" fullWidth />
+        <Typography tooltip="This is a tooltip">Daily maximum of 30 minutes. And a lot of really long text.     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </Typography>
+      </Box>
+    )}
+  >
+    <Button text="Trigger me" />
+  </Popover>
+);
--- packages/syntax-floating-components/src/Popover/Popover.tsx
+++ packages/syntax-floating-components/src/Popover/Popover.tsx
@@ -1,263 +1,387 @@
-import * as React from "react";
-import classNames from "classnames";
-import {
-  useFloating,
-  autoUpdate,
-  offset,
-  flip,
-  shift,
-  useHover,
-  useFocus,
-  useDismiss,
-  useRole,
-  useInteractions,
-  useMergeRefs,
-  arrow,
-  size,
-} from "@floating-ui/react";
-import type { Side, Strategy, UseFloatingReturn } from "@floating-ui/react";
-
+import React, { type ReactNode, forwardRef, type ReactElement, useEffect } from "react";
+import { Box, Typography } from "@cambly/syntax-core";
 import styles from "./Popover.module.css";
-import elevationStyles from "../../../syntax-core/src/elevation/elevation.module.css";
 
-type PopoverOptions = {
+import {
+  usePopoverStore,
+  Popover as AriakitPopover,
+  PopoverDisclosure,
+  PopoverArrow,
+} from '@ariakit/react';
+import useForwardFocus from "../ariakit-utils/useForwardFocus";
+import useChangeContentVisibility from "../ariakit-utils/useChangeContentVisibility";
+
+type PopoverProps = {
+  /** Optional aria-label for the popover (content element) */
+  accessibilityLabel?: string;
+  /** Required trigger element */
+  children: ReactElement | string;
+  /* Content to be shown inside the popover. */
+  content: ReactNode;
   /**
-   * How long a user hovers before Popover shows (in ms)
-   *
-   * @defaultValue 0
-   */
-  delay?: number;
-  /**
-   * Whether an (uncontrolled) Popover should open on load
-   *
+   * If set to true the popover will render initially open
    * @defaultValue false
    */
   initialOpen?: boolean;
-  /**
-   * Value of 'open' state of the Popover content if controlled
-   */
+  /** Optional handler for change of visibility for popover content.  called with (visible: boolean) */
+  onChangeContentVisibility?: (visible: boolean) => void;
+  /** Optional boolean to control open state of popover externally */
   open?: boolean;
   /**
-   * Function to set value of 'open' state to true if controlled
+   * Location of the popover content relative to anchor element
+   * @defaultValue "top-start"
    */
-  onOpen?: (open: boolean) => void;
-  /**
-   * Function to set value of 'open' state to false if controlled
-   */
-  onClose?: (close: boolean) => void;
-  /**
-   * Location of the Popover content relative to anchor element
-   *
-   * @defaultValue "right"
-   */
-  placement?: Side;
-  /**
-   * Placement strategy, either "fixed" or "absolute"
-   *
-   * @defaultValue "absolute"
-   */
-  strategy?: Strategy;
-};
-
-export function usePopover({
-  delay = 0,
-  initialOpen = false,
-  open: controlledOpen,
-  placement = "right",
-  strategy = "absolute",
-  onOpen = undefined,
-  onClose = undefined,
-}: PopoverOptions): UseFloatingReturn & {
-  getReferenceProps: (
-    userProps?: React.HTMLProps<Element> | undefined,
-  ) => Record<string, unknown>;
-  getFloatingProps: (
-    userProps?: React.HTMLProps<HTMLElement> | undefined,
-  ) => Record<string | number | symbol, unknown>;
-  getItemProps: (
-    userProps?: React.HTMLProps<HTMLElement> | undefined,
-  ) => Record<string | number | symbol, unknown>;
-  arrowRef: React.MutableRefObject<null>;
-  open: boolean;
-  setOpen: (open: boolean) => void;
-} {
-  const [uncontrolledOpen, setUncontrolledOpen] = React.useState(initialOpen);
-
-  const arrowRef = React.useRef(null);
-
-  const open = controlledOpen ?? uncontrolledOpen;
-  let setOpen: (open: boolean) => void;
-  if (onOpen && controlledOpen == true) {
-    setOpen = onOpen;
-  } else if (onClose && controlledOpen == false) {
-    setOpen = onClose;
-  } else {
-    setOpen = setUncontrolledOpen;
-  }
-
-  const data = useFloating({
-    placement,
-    strategy,
-    open,
-    onOpenChange: setOpen,
-    whileElementsMounted: autoUpdate,
-    middleware: [
-      offset(8),
-      flip({
-        fallbackAxisSideDirection: "start",
-      }),
-      shift({ padding: 4 }),
-      arrow({ element: arrowRef }),
-      size({
-        apply({ availableHeight, elements }) {
-          Object.assign(elements.floating.style, {
-            maxHeight: `${availableHeight}px`,
-          });
-        },
-      }),
-    ],
-  });
-
-  const context = data.context;
-
-  const hover = useHover(context, {
-    move: false,
-    enabled: controlledOpen == null,
-    delay,
-  });
-  const focus = useFocus(context, {
-    enabled: controlledOpen == null,
-  });
-  const dismiss = useDismiss(context);
-  const role = useRole(context, { role: "dialog" });
-
-  const interactions = useInteractions([hover, focus, dismiss, role]);
-
-  return React.useMemo(
-    () => ({
-      open,
-      setOpen,
-      ...interactions,
-      ...data,
-      arrowRef,
-    }),
-    [open, setOpen, interactions, data],
-  );
+  placement?: "top" | "bottom" | "top-start" | "top-end" | "bottom-start" | "bottom-end";
 }
 
-const PopoverContext = React.createContext<ReturnType<
-  typeof usePopover
-> | null>(null);
-
-const usePopoverContext = () => {
-  const context = React.useContext(PopoverContext);
-
-  if (context == null) {
-    throw new Error("Popover components must be wrapped in <Popover />");
-  }
-
-  return context;
-};
-
 /**
- * [Popover](https://cambly-syntax.vercel.app/?path=/docs/floating-components-Popover--docs) is a floating component that can be filled with anything.
+ * [Tooltip](https://cambly-syntax.vercel.app/?path=/docs/floating-components-popover--docs) displays contextual information on hover or focus.
  *
- * Most likely will be using the controlled variation for showing/hiding components. For detailed implementation details, see `See Code` in `Controlled Popover`.
- *
- * Popover content takes in props of width and maxWidth so you can control what size you want it to be.
- * Padding is set to `28px`
- * Would recommend passing in a `Box` component into PopoverContent to control spacing and positioning.
+ * Tooltip content is hidden by default and shown on hover or focus.
+ * The content is hidden again when the user mouses out of the trigger element or blurs the trigger element or presses Escape
  *
  * Example Usage:
  ```
-<Popover
-  delay={0}
-  placement="bottom"
-  initialOpen
-  strategy="absolute"
->
-  <PopoverTrigger>
-    <IconButton />
-  </PopoverTrigger>
-  <PopoverContent maxWidth="500px" width="100%">
-    <Box>
-      // content goes here
-    </Box>
-  </PopoverContent>
-</Popover/>
+  <Popover
+    placement="bottom-start"
+    initialOpen
+    content={(
+      <Box padding={2} maxWidth={400}>
+        ... some content goes here
+      </Box>
+    )}
+  >
+      <Button text="Trigger me" />
+  </Popover>
  ```
  */
-export function Popover({
-  children,
-  ...options
-}: { children: React.ReactNode } & PopoverOptions): JSX.Element {
-  // This can accept any props as options, e.g. `placement`,
-  // or other positioning options.
-  const popover = usePopover(options);
-  return (
-    <PopoverContext.Provider value={popover}>
-      {children}
-    </PopoverContext.Provider>
-  );
-}
+export default forwardRef<HTMLDivElement, PopoverProps>(function Popover(props: PopoverProps, ref): ReactElement {
+  const {
+    accessibilityLabel,
+    children,
+    content,
+    initialOpen = false,
+    onChangeContentVisibility,
+    open,
+    placement = "top-start",
+  } = props;
 
-export const PopoverTrigger = React.forwardRef<
-  HTMLElement,
-  React.HTMLProps<HTMLElement>
->(function PopoverTrigger({ children, ...props }, propRef) {
-  const context = usePopoverContext();
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment
-  const childrenRef = (children as any).ref;
-  const ref = useMergeRefs([
-    context.refs.setReference,
-    propRef,
-    childrenRef,
-  ] as React.Ref<unknown>[]);
+  const store = usePopoverStore({
+    defaultOpen: initialOpen,
+    placement,
+  });
 
-  if (React.isValidElement(children)) {
-    return React.cloneElement(
-      children,
-      context.getReferenceProps({
-        ref,
-        ...props,
-        ...children.props,
-        "data-state": context.open ? "open" : "closed",
-      } as React.HTMLProps<Element> | undefined),
-    );
-  } else {
-    throw new Error(
-      "PopoverTrigger must be wrapped around a valid React element",
-    );
-  }
-});
-
-export const PopoverContent = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLProps<HTMLDivElement> & {
-    maxWidth?: string | number;
-    width?: string | number;
-  }
->(function PopoverContent(props, propRef) {
-  const { ...context } = usePopoverContext();
-  const ref = useMergeRefs([context.refs.setFloating, propRef]);
-
-  if (!context.open) return null;
+  // For Analytics and Control:
+  useChangeContentVisibility(store, onChangeContentVisibility);
+  // transfer focus to child element if it is focusable. Wire into onFocus on Tooltip Anchor
+  const forwardAnchorFocusToInteractiveChild = useForwardFocus(store);
+  // allow external control of open state via `open` prop
+  useEffect(() => {
+    if (open === undefined) return;
+    store.setOpen(open);
+  }, [open, store]);
 
   return (
-    <div
-      {...context.getFloatingProps(props)}
-      ref={ref}
-      className={classNames(
-        styles.popoverContent,
-        elevationStyles.elevation400BoxShadow,
-      )}
-      style={{
-        ...context.floatingStyles,
-        width: props.width,
-        maxWidth: props.maxWidth,
-      }}
-    >
-      {props.children}
-    </div>
+    <>
+      <PopoverDisclosure store={store} ref={ref}
+        as="span"
+        className={styles.trigger}
+        onFocus={forwardAnchorFocusToInteractiveChild}
+      >
+        {typeof children === 'string' ? <Typography color="inherit">{children}</Typography> : children}
+      </PopoverDisclosure>
+      <AriakitPopover store={store}
+        gutter={0}
+        overflowPadding={4}
+        fitViewport
+        className={styles.content}
+        // ensures focusable items in popover are tab-able
+        // preserveTabOrder
+        // tabIndex={0}
+        // first thing screen reader reads
+        // e.g. "<this was the label prop>, dialog, 4 items..."
+        aria-label={accessibilityLabel}
+      // ariakit sets role
+      >
+        <>
+          <PopoverArrow />
+          <Box
+            padding={7}
+            position="relative"
+            width="100%"
+            height="var(--popover-available-height, 100%)"
+            dangerouslySetInlineStyle={{ __style: { overflowY: 'auto' } }}
+          >
+            <Typography inline size={100} color="inherit">
+              {content}
+            </Typography>
+          </Box>
+        </>
+      </AriakitPopover >
+    </>
   );
-});
+})
+
+// import * as React from "react";
+// import classNames from "classnames";
+// import {
+//   useFloating,
+//   autoUpdate,
+//   offset,
+//   flip,
+//   shift,
+//   useHover,
+//   useFocus,
+//   useDismiss,
+//   useRole,
+//   useInteractions,
+//   useMergeRefs,
+//   arrow,
+//   size,
+// } from "@floating-ui/react";
+// import type { Side, Strategy, UseFloatingReturn } from "@floating-ui/react";
+
+// import styles from "./Popover.module.css";
+// import elevationStyles from "../../../syntax-core/src/elevation/elevation.module.css";
+
+// type PopoverOptions = {
+//   /**
+//    * How long a user hovers before Popover shows (in ms)
+//    *
+//    * @defaultValue 0
+//    */
+//   delay?: number;
+//   /**
+//    * Whether an (uncontrolled) Popover should open on load
+//    *
+//    * @defaultValue false
+//    */
+//   initialOpen?: boolean;
+//   /**
+//    * Value of 'open' state of the Popover content if controlled
+//    */
+//   open?: boolean;
+//   /**
+//    * Function to set value of 'open' state to true if controlled
+//    */
+//   onOpen?: (open: boolean) => void;
+//   /**
+//    * Function to set value of 'open' state to false if controlled
+//    */
+//   onClose?: (close: boolean) => void;
+//   /**
+//    * Location of the Popover content relative to anchor element
+//    *
+//    * @defaultValue "right"
+//    */
+//   placement?: Side;
+//   /**
+//    * Placement strategy, either "fixed" or "absolute"
+//    *
+//    * @defaultValue "absolute"
+//    */
+//   strategy?: Strategy;
+// };
+
+// export function usePopover({
+//   delay = 0,
+//   initialOpen = false,
+//   open: controlledOpen,
+//   placement = "right",
+//   strategy = "absolute",
+//   onOpen = undefined,
+//   onClose = undefined,
+// }: PopoverOptions): UseFloatingReturn & {
+//   getReferenceProps: (
+//     userProps?: React.HTMLProps<Element> | undefined,
+//   ) => Record<string, unknown>;
+//   getFloatingProps: (
+//     userProps?: React.HTMLProps<HTMLElement> | undefined,
+//   ) => Record<string | number | symbol, unknown>;
+//   getItemProps: (
+//     userProps?: React.HTMLProps<HTMLElement> | undefined,
+//   ) => Record<string | number | symbol, unknown>;
+//   arrowRef: React.MutableRefObject<null>;
+//   open: boolean;
+//   setOpen: (open: boolean) => void;
+// } {
+//   const [uncontrolledOpen, setUncontrolledOpen] = React.useState(initialOpen);
+
+//   const arrowRef = React.useRef(null);
+
+//   const open = controlledOpen ?? uncontrolledOpen;
+//   let setOpen: (open: boolean) => void;
+//   if (onOpen && controlledOpen == true) {
+//     setOpen = onOpen;
+//   } else if (onClose && controlledOpen == false) {
+//     setOpen = onClose;
+//   } else {
+//     setOpen = setUncontrolledOpen;
+//   }
+
+//   const data = useFloating({
+//     placement,
+//     strategy,
+//     open,
+//     onOpenChange: setOpen,
+//     whileElementsMounted: autoUpdate,
+//     middleware: [
+//       offset(8),
+//       flip({
+//         fallbackAxisSideDirection: "start",
+//       }),
+//       shift({ padding: 4 }),
+//       arrow({ element: arrowRef }),
+//       size({
+//         apply({ availableHeight, elements }) {
+//           Object.assign(elements.floating.style, {
+//             maxHeight: `${availableHeight}px`,
+//           });
+//         },
+//       }),
+//     ],
+//   });
+
+//   const context = data.context;
+
+//   const hover = useHover(context, {
+//     move: false,
+//     enabled: controlledOpen == null,
+//     delay,
+//   });
+//   const focus = useFocus(context, {
+//     enabled: controlledOpen == null,
+//   });
+//   const dismiss = useDismiss(context);
+//   const role = useRole(context, { role: "dialog" });
+
+//   const interactions = useInteractions([hover, focus, dismiss, role]);
+
+//   return React.useMemo(
+//     () => ({
+//       open,
+//       setOpen,
+//       ...interactions,
+//       ...data,
+//       arrowRef,
+//     }),
+//     [open, setOpen, interactions, data],
+//   );
+// }
+
+// const PopoverContext = React.createContext<ReturnType<
+//   typeof usePopover
+// > | null>(null);
+
+// const usePopoverContext = () => {
+//   const context = React.useContext(PopoverContext);
+
+//   if (context == null) {
+//     throw new Error("Popover components must be wrapped in <Popover />");
+//   }
+
+//   return context;
+// };
+
+// /**
+//  * [Popover](https://cambly-syntax.vercel.app/?path=/docs/floating-components-Popover--docs) is a floating component that can be filled with anything.
+//  *
+//  * Most likely will be using the controlled variation for showing/hiding components. For detailed implementation details, see `See Code` in `Controlled Popover`.
+//  *
+//  * Popover content takes in props of width and maxWidth so you can control what size you want it to be.
+//  * Padding is set to `28px`
+//  * Would recommend passing in a `Box` component into PopoverContent to control spacing and positioning.
+//  *
+//  * Example Usage:
+//  ```
+// <Popover
+//   delay={0}
+//   placement="bottom"
+//   initialOpen
+//   strategy="absolute"
+// >
+//   <PopoverTrigger>
+//     <IconButton />
+//   </PopoverTrigger>
+//   <PopoverContent maxWidth="500px" width="100%">
+//     <Box>
+//       // content goes here
+//     </Box>
+//   </PopoverContent>
+// </Popover/>
+//  ```
+//  */
+// export function Popover({
+//   children,
+//   ...options
+// }: { children: React.ReactNode } & PopoverOptions): JSX.Element {
+//   // This can accept any props as options, e.g. `placement`,
+//   // or other positioning options.
+//   const popover = usePopover(options);
+//   return (
+//     <PopoverContext.Provider value={popover}>
+//       {children}
+//     </PopoverContext.Provider>
+//   );
+// }
+
+// export const PopoverTrigger = React.forwardRef<
+//   HTMLElement,
+//   React.HTMLProps<HTMLElement>
+// >(function PopoverTrigger({ children, ...props }, propRef) {
+//   const context = usePopoverContext();
+//   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment
+//   const childrenRef = (children as any).ref;
+//   const ref = useMergeRefs([
+//     context.refs.setReference,
+//     propRef,
+//     childrenRef,
+//   ] as React.Ref<unknown>[]);
+
+//   if (React.isValidElement(children)) {
+//     return React.cloneElement(
+//       children,
+//       context.getReferenceProps({
+//         ref,
+//         ...props,
+//         ...children.props,
+//         "data-state": context.open ? "open" : "closed",
+//       } as React.HTMLProps<Element> | undefined),
+//     );
+//   } else {
+//     throw new Error(
+//       "PopoverTrigger must be wrapped around a valid React element",
+//     );
+//   }
+// });
+
+// export const PopoverContent = React.forwardRef<
+//   HTMLDivElement,
+//   React.HTMLProps<HTMLDivElement> & {
+//     maxWidth?: string | number;
+//     width?: string | number;
+//   }
+// >(function PopoverContent(props, propRef) {
+//   const { ...context } = usePopoverContext();
+//   const ref = useMergeRefs([context.refs.setFloating, propRef]);
+
+//   if (!context.open) return null;
+
+//   return (
+//     <div
+//       {...context.getFloatingProps(props)}
+//       ref={ref}
+//       className={classNames(
+//         styles.popoverContent,
+//         elevationStyles.elevation400BoxShadow,
+//       )}
+//       style={{
+//         ...context.floatingStyles,
+//         width: props.width,
+//         maxWidth: props.maxWidth,
+//       }}
+//     >
+//       {props.children}
+//     </div>
+//   );
+// });
