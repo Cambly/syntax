b541ee2dc1e0a92061ee3dcaa800e8403f50de6e
Wilson Yu <hello@wilsonyu.io>
2024-01-11 13:36:02 -0800

    AvatarGroup: update AvatarGroup API using context (#295)
    * update AvatarGroup API using context
    
    * update avatar icon positioning
    
    * update comments + tests
    
    * update css
    
    * update docs
    
    * add use client for avatar group + avatar
    
    * fix dot positioning
    
    * remove use client
    
    * re-add display name

--- packages/syntax-core/src/Avatar/Avatar.tsx
+++ packages/syntax-core/src/Avatar/Avatar.tsx
@@ -1,13 +1,63 @@
+import { type ReactElement } from "react";
 import classNames from "classnames";
 import styles from "./Avatar.module.css";
 import Box from "../Box/Box";
+import { useAvatarGroup } from "../AvatarGroup/AvatarGroup";
 
 const sizeToIconStyles = {
-  sm: { bottom: 8, marginInlineEnd: 0, height: 4, width: 4 },
-  md: { bottom: 8, marginInlineEnd: 0, height: 8, width: 8 },
-  lg: { bottom: 8, marginInlineEnd: 4, height: 12, width: 12 },
+  sm: { bottom: 6, marginInlineEnd: 2, height: 4, width: 4 },
+  md: { bottom: 6, marginInlineEnd: 2, height: 8, width: 8 },
+  lg: { bottom: 6, marginInlineEnd: 6, height: 12, width: 12 },
   xl: { bottom: 12, marginInlineEnd: 12, height: 16, width: 16 },
 } as const;
+
+const sizeToMargin = {
+  sm: -16,
+  md: -28,
+  lg: -48,
+  xl: -88,
+} as const;
+
+function AvatarInternal({
+  accessibilityLabel,
+  icon,
+  size = "md",
+  src,
+}: {
+  accessibilityLabel: string;
+  icon?: React.ReactElement;
+  size?: "sm" | "md" | "lg" | "xl";
+  src: string;
+}): ReactElement {
+  return (
+    <div className={classNames(styles.avatar, styles[size])}>
+      <img
+        alt={accessibilityLabel}
+        src={src}
+        className={classNames(styles.avatarImage, styles[size])}
+      />
+      {icon && (
+        <Box display="flex" position="relative" justifyContent="end">
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            position="absolute"
+            dangerouslySetInlineStyle={{
+              __style: {
+                border: "1px solid white",
+                ...sizeToIconStyles[size],
+              },
+            }}
+            rounding="full"
+          >
+            {icon}
+          </Box>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 /**
  * [Avatar](https://cambly-syntax.vercel.app/?path=/docs/components-avatar--docs) is a circular image that represents a user.
@@ -43,30 +93,43 @@ const Avatar = ({
    */
   src: string;
 }): JSX.Element => {
-  return (
-    <div className={styles[size]}>
-      <img
-        alt={accessibilityLabel}
-        src={src}
-        className={classNames(styles.avatar, styles[size])}
-      />
-      {icon && (
-        <Box display="flex" position="relative" justifyContent="end">
-          <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            position="absolute"
-            dangerouslySetInlineStyle={{
-              __style: { border: "1px solid white", ...sizeToIconStyles[size] },
-            }}
-            rounding="full"
-          >
-            {icon}
-          </Box>
+  const avatarGroupContext = useAvatarGroup();
+
+  if (avatarGroupContext !== null) {
+    return (
+      <Box
+        position="relative"
+        dangerouslySetInlineStyle={{
+          __style: {
+            marginInlineEnd: sizeToMargin[avatarGroupContext.size],
+          },
+        }}
+      >
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          backgroundColor="white"
+          rounding="full"
+        >
+          <AvatarInternal
+            accessibilityLabel={accessibilityLabel}
+            icon={icon}
+            size={avatarGroupContext.size}
+            src={src}
+          />
         </Box>
-      )}
-    </div>
+      </Box>
+    );
+  }
+
+  return (
+    <AvatarInternal
+      accessibilityLabel={accessibilityLabel}
+      icon={icon}
+      size={size}
+      src={src}
+    />
   );
 };
 
--- packages/syntax-core/src/AvatarGroup/AvatarGroup.tsx
+++ packages/syntax-core/src/AvatarGroup/AvatarGroup.tsx
@@ -1,40 +1,87 @@
+"use client";
+
+import {
+  type ReactNode,
+  createContext,
+  useContext,
+  type ReactElement,
+} from "react";
 import Box from "../Box/Box";
-import { type ReactNode } from "react";
-import AvatarGroupAvatar from "./AvatarGroupAvatar";
+
+type Size = "sm" | "md" | "lg" | "xl";
+type Orientation = "standard" | "reverse";
+
+type AvatarGroupContextType = {
+  size: Size;
+  orientation: Orientation;
+};
+
+const AvatarGroupContext = createContext<AvatarGroupContextType | null>(null);
+
+export function useAvatarGroup(): AvatarGroupContextType | null {
+  const context = useContext(AvatarGroupContext);
+  return !context ? null : context;
+}
 
 /**
  * [AvatarGroup](https://cambly-syntax.vercel.app/?path=/docs/components-avatargroup--docs) is a stack of avatars to represent a group of people
+ *
+ * Pass in Avatar components as children to the AvatarGroup component. The size prop that is passed into the AvatarGroup component will override Avatar's size prop..
+ *
+ * Usage:
+ *
+ * <AvatarGroup size="xl" orientation="standard">
+ *   <Avatar accessibilityLabel="Joseph Liotta" src="image.png" />
+ *   <Avatar accessibilityLabel="Joseph Liotta" src="image.png" />
+ *   <Avatar accessibilityLabel="Joseph Liotta" src="image.png" />
+ * </AvatarGroup>
+ *
  */
-
 export default function AvatarGroup({
-  children,
+  size = "md",
   orientation = "standard",
+  children,
 }: {
   /**
-   * One or more.Avatar components.
+   * Size of the avatars in the AvatarGroup.
+   *
+   * * `sm`: 24px
+   * * `md`: 40px
+   * * `lg`: 72px
+   * * `xl`: 128px
+   *
+   * @defaultValue `md`
+   */
+  size?: Size;
+  /**
+   * Orientation of the AvatarGroup.
+   * This describes the order of rendering of the Avatar components.
+   * Standard renders the Avatar components with the right component on top.
+   * Reverse renders the Avatar components with the left component on top.
+   *
+   * @defaultValue `standard`
+   */
+  orientation?: Orientation;
+  /**
+   * Avatars to be rendered within the AvatarGroup.
    */
   children: ReactNode;
-  /**
-   * Direction of the avatar stack
-   * standard - avatars stack from right to left
-   * reverse - avatars stack from left to right
-   */
-  orientation?: "standard" | "reverse";
-}): JSX.Element {
+}): ReactElement {
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      dangerouslySetInlineStyle={{
-        __style: {
-          flexDirection: orientation === "reverse" ? "row-reverse" : "row",
-        },
-      }}
-    >
-      {children}
-    </Box>
+    <AvatarGroupContext.Provider value={{ size, orientation }}>
+      <Box
+        display="flex"
+        justifyContent={orientation === "standard" ? "start" : "end"}
+        dangerouslySetInlineStyle={{
+          __style: {
+            flexDirection: orientation === "standard" ? "row" : "row-reverse",
+          },
+        }}
+      >
+        {children}
+      </Box>
+    </AvatarGroupContext.Provider>
   );
 }
 
 AvatarGroup.displayName = "AvatarGroup";
-AvatarGroup.Avatar = AvatarGroupAvatar;
