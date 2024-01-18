681c7acb8359d3d1518646a7eb2e8896e342ebc4
benjroy <ben.allen@cambly.com>
2024-01-17 20:15:38 -0800

    Feat: Typography forwards ref, ensures compatibility with Triggerable

--- packages/syntax-core/src/Typography/Typography.tsx
+++ packages/syntax-core/src/Typography/Typography.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import { type ReactElement, type ReactNode } from "react";
+import { forwardRef, type ReactElement, type ReactNode } from "react";
 import { type Color } from "../constants";
 import styles from "./Typography.module.css";
 import colorStyles from "../colors/colors.module.css";
@@ -26,95 +26,101 @@ function textColor(color: (typeof Color)[number]): string {
 /**
  * [Typography](https://cambly-syntax.vercel.app/?path=/docs/components-typography--docs) is a component that renders text.
  */
-const Typography = ({
-  align = "start",
-  as = "div",
-  children,
-  color = "gray900",
-  "data-testid": dataTestId,
-  inline = false,
-  lineClamp = undefined,
-  size = 200,
-  tooltip,
-  transform = "none",
-  underline = false,
-  weight = "regular",
-}: {
-  /**
-   * Aligns the text to the left, right, or center of the container.
-   * * `start` and `end` will align the text to the left or right of the container depending on the locale.
-   * * `forceLeft` and `forceRight` will align the text to the left or right of the container regardless of the locale (should be used sparingly).
-   *
-   * @defaultValue "start"
-   */
-  align?: "start" | "end" | "forceLeft" | "center" | "forceRight";
-  /**
-   * DOM element to render as.
-   *
-   * @defaultValue "div"
-   */
-  as?: "div" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-  /**
-   * The text to be rendered
-   */
-  children?: ReactNode;
-  /**
-   * The color of the text.
-   *
-   * @defaultValue "gray900"
-   */
-  color?: (typeof Color)[number];
-  /**
-   * Test id for the text
-   */
-  "data-testid"?: string;
-  /**
-   * Whether the text should flow inline with other elements.
-   *
-   * @defaultValue false
-   */
-  inline?: boolean;
-  /**
-   * The number of lines we should truncate the text at
-   */
-  lineClamp?: number | undefined;
-  /**
-   * Size of the text.
-   *
-   * * `100`: 12px
-   * * `200`: 14px
-   * * `300`: 16px
-   * * `500`: 20px
-   * * `600`: 28px
-   * * `700`: 40px
-   * * `800`: 64px
-   *
-   * @defaultValue 200
-   */
-  size?: 100 | 200 | 300 | 500 | 600 | 700 | 800;
-  /**
-   * The tooltip to be displayed when the user hovers the text
-   */
-  tooltip?: string;
-  /**
-   * Whether the text should be transformed to uppercase.
-   *
-   * @defaultValue "none"
-   */
-  transform?: "none" | "uppercase";
-  /**
-   * Whether the text has an underline.
-   *
-   * @defaultValue false
-   */
-  underline?: boolean;
-  /**
-   * Indicates the boldness of the text.
-   *
-   * @defaultValue "regular"
-   */
-  weight?: "regular" | "interactive" | "semiBold" | "bold" | "heavy";
-}): ReactElement => {
+const Typography = forwardRef<
+  HTMLDivElement,
+  {
+    /**
+     * Aligns the text to the left, right, or center of the container.
+     * * `start` and `end` will align the text to the left or right of the container depending on the locale.
+     * * `forceLeft` and `forceRight` will align the text to the left or right of the container regardless of the locale (should be used sparingly).
+     *
+     * @defaultValue "start"
+     */
+    align?: "start" | "end" | "forceLeft" | "center" | "forceRight";
+    /**
+     * DOM element to render as.
+     *
+     * @defaultValue "div"
+     */
+    as?: "div" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+    /**
+     * The text to be rendered
+     */
+    children?: ReactNode;
+    /**
+     * The color of the text.
+     *
+     * @defaultValue "gray900"
+     */
+    color?: (typeof Color)[number];
+    /**
+     * Test id for the text
+     */
+    "data-testid"?: string;
+    /**
+     * Whether the text should flow inline with other elements.
+     *
+     * @defaultValue false
+     */
+    inline?: boolean;
+    /**
+     * The number of lines we should truncate the text at
+     */
+    lineClamp?: number | undefined;
+    /**
+     * Size of the text.
+     *
+     * * `100`: 12px
+     * * `200`: 14px
+     * * `300`: 16px
+     * * `500`: 20px
+     * * `600`: 28px
+     * * `700`: 40px
+     * * `800`: 64px
+     *
+     * @defaultValue 200
+     */
+    size?: 100 | 200 | 300 | 500 | 600 | 700 | 800;
+    /**
+     * The tooltip to be displayed when the user hovers the text
+     */
+    tooltip?: string;
+    /**
+     * Whether the text should be transformed to uppercase.
+     *
+     * @defaultValue "none"
+     */
+    transform?: "none" | "uppercase";
+    /**
+     * Whether the text has an underline.
+     *
+     * @defaultValue false
+     */
+    underline?: boolean;
+    /**
+     * Indicates the boldness of the text.
+     *
+     * @defaultValue "regular"
+     */
+    weight?: "regular" | "interactive" | "semiBold" | "bold" | "heavy";
+  }
+>(function Typography(
+  {
+    align = "start",
+    as = "div",
+    children,
+    color = "gray900",
+    "data-testid": dataTestId,
+    inline = false,
+    lineClamp = undefined,
+    size = 200,
+    tooltip,
+    transform = "none",
+    underline = false,
+    weight = "regular",
+  },
+  ref,
+): ReactElement {
   const Tag = as;
 
   return (
@@ -135,10 +141,11 @@ const Typography = ({
         WebkitLineClamp: lineClamp,
       }}
       title={tooltip}
+      ref={ref}
     >
       {children}
     </Tag>
   );
-};
+});
 
 export default Typography;
