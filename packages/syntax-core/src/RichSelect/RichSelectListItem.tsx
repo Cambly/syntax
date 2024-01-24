8a958777b4c9ae16f7b18ffb2a9cb8b927513e44
benjroy <ben.allen@cambly.com>
2024-01-23 22:19:00 -0800

    WIP: RichSelectListItem extracted from RichSelectChip

---
+++ packages/syntax-core/src/RichSelect/RichSelectListItem.tsx
@@ -0,0 +1,49 @@
+import React, { forwardRef, type ReactNode, type ReactElement } from "react";
+import {
+  ListBoxItem as ReactAriaListBoxItem,
+  type ListBoxItemRenderProps,
+} from "react-aria-components";
+import styles from "./RichSelect.module.css";
+import { useDisableKey, useSelectKey } from "./DisabledKeysProvider";
+
+export type RichSelectListItemProps = {
+  "data-testid"?: string;
+  value: string;
+  label: string;
+  disabled?: boolean;
+  selected?: boolean;
+  /** The children of the component. A function may be provided to alter the children based on component state. */
+  children?: ReactNode | ((values: ListBoxItemRenderProps) => ReactNode);
+};
+
+const RichSelectListItem = forwardRef<
+  HTMLDivElement,
+  RichSelectListItemProps & {
+    /** The children of the component. A function may be provided to alter the children based on component state. */
+    children?: ReactNode | ((values: ListBoxItemRenderProps) => ReactNode);
+  }
+>(function RichSelectListItem(props, ref): ReactElement {
+  const {
+    "data-testid": dataTestId,
+    value,
+    label,
+    disabled = false,
+    selected = false,
+    children,
+  } = props;
+  useDisableKey(value, disabled);
+  useSelectKey(value, selected);
+  return (
+    <ReactAriaListBoxItem
+      ref={ref}
+      id={value}
+      textValue={label}
+      className={styles.listBoxItem}
+      data-testid={dataTestId}
+    >
+      {children}
+    </ReactAriaListBoxItem>
+  );
+});
+
+export default RichSelectListItem;
