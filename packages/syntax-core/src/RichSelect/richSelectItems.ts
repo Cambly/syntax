5356c35d7376314940bc319781af1795eb3e8285
benjroy <ben.allen@cambly.com>
2024-01-31 17:58:42 -0800

    WIP: src/RichSelect/richSelectItems.ts, swuashme with composed disable attributes commit

---
+++ packages/syntax-core/src/RichSelect/richSelectItems.ts
@@ -0,0 +1,11 @@
+import RichSelectOptGroup from "./RichSelectOptGroup";
+import RichSelectChip from "./RichSelectChip";
+import RichSelectRadioButton from "./RichSelectRadioButton";
+
+const richSelectItems = {
+  OptGroup: RichSelectOptGroup,
+  Chip: RichSelectChip,
+  RadioButton: RichSelectRadioButton,
+};
+
+export default richSelectItems;
