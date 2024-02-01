7f2b9068e4ed2e430c7b0877dcd0a2ce69b87660
benjroy <ben.allen@cambly.com>
2024-01-31 18:46:21 -0800

    Server tests for RSB + RSL

---
+++ packages/syntax-core/src/RichSelect/RichSelectBox.server.test.tsx
@@ -0,0 +1,78 @@
+/**
+ * @vitest-environment node
+ */
+import RichSelectBox from "./RichSelectBox";
+import { renderToString } from "react-dom/server";
+
+const defaultRequiredProps = {
+  onChange: () => undefined,
+  label: "x",
+};
+
+describe("richselectlist - server", () => {
+  it("can render chips", () => {
+    const renderOnServer = () =>
+      renderToString(
+        <RichSelectBox {...defaultRequiredProps}>
+          <RichSelectBox.Chip label="Opt1" value="opt1" />
+          <RichSelectBox.Chip label="Opt2" value="opt2" />
+          <RichSelectBox.Chip label="Opt3" value="opt3" />
+        </RichSelectBox>,
+      );
+    expect(renderOnServer).not.toThrow();
+    expect(renderOnServer()).toContain("Opt1");
+    expect(renderOnServer()).toContain("Opt2");
+    expect(renderOnServer()).toContain("Opt3");
+  });
+
+  it("can render sections", () => {
+    const renderOnServer = () =>
+      renderToString(
+        <RichSelectBox {...defaultRequiredProps}>
+          <RichSelectBox.Section label="section1">
+            <RichSelectBox.Chip label="Opt1" value="opt1" />
+          </RichSelectBox.Section>
+          <RichSelectBox.Section label="section2">
+            <RichSelectBox.Chip label="Opt2" value="opt2" />
+          </RichSelectBox.Section>
+          <RichSelectBox.Section label="section3">
+            <RichSelectBox.Chip label="Opt3" value="opt3" />
+          </RichSelectBox.Section>
+        </RichSelectBox>,
+      );
+    expect(renderOnServer).not.toThrow();
+    expect(renderOnServer()).toContain("section1");
+    expect(renderOnServer()).toContain("section2");
+    expect(renderOnServer()).toContain("section3");
+    expect(renderOnServer()).toContain("Opt1");
+    expect(renderOnServer()).toContain("Opt2");
+    expect(renderOnServer()).toContain("Opt3");
+  });
+
+  it("can render RadioButtons", () => {
+    const renderOnServer = () =>
+      renderToString(
+        <RichSelectBox {...defaultRequiredProps}>
+          <RichSelectBox.RadioButton
+            label="Opt1"
+            value="opt1"
+            name="opt1name"
+          />
+          <RichSelectBox.RadioButton
+            label="Opt2"
+            value="opt2"
+            name="opt2name"
+          />
+          <RichSelectBox.RadioButton
+            label="Opt3"
+            value="opt3"
+            name="opt3name"
+          />
+        </RichSelectBox>,
+      );
+    expect(renderOnServer).not.toThrow();
+    expect(renderOnServer()).toContain("Opt1");
+    expect(renderOnServer()).toContain("Opt2");
+    expect(renderOnServer()).toContain("Opt3");
+  });
+});
---
+++ packages/syntax-core/src/RichSelect/RichSelectList.server.test.tsx
@@ -0,0 +1,81 @@
+/**
+ * @vitest-environment node
+ */
+import RichSelectList from "./RichSelectList";
+import { renderToString } from "react-dom/server";
+
+const defaultRequiredProps = {
+  onChange: () => undefined,
+  label: "x",
+};
+
+describe("richselectbox - server", () => {
+  it("can render chips", () => {
+    const renderOnServer = () =>
+      renderToString(
+        <RichSelectList {...defaultRequiredProps}>
+          <RichSelectList.Chip label="Opt1" value="opt1" />
+          <RichSelectList.Chip label="Opt2" value="opt2" />
+          <RichSelectList.Chip label="Opt3" value="opt3" />
+        </RichSelectList>,
+      );
+    expect(renderOnServer).not.toThrow();
+    // popover is hidden, not in document
+    expect(renderOnServer()).not.toContain("Opt1");
+    expect(renderOnServer()).not.toContain("Opt2");
+    expect(renderOnServer()).not.toContain("Opt3");
+  });
+
+  it("can render sections", () => {
+    const renderOnServer = () =>
+      renderToString(
+        <RichSelectList {...defaultRequiredProps}>
+          <RichSelectList.Section label="section1">
+            <RichSelectList.Chip label="Opt1" value="opt1" />
+          </RichSelectList.Section>
+          <RichSelectList.Section label="section2">
+            <RichSelectList.Chip label="Opt2" value="opt2" />
+          </RichSelectList.Section>
+          <RichSelectList.Section label="section3">
+            <RichSelectList.Chip label="Opt3" value="opt3" />
+          </RichSelectList.Section>
+        </RichSelectList>,
+      );
+    expect(renderOnServer).not.toThrow();
+    // popover is hidden, not in document
+    expect(renderOnServer()).not.toContain("section1");
+    expect(renderOnServer()).not.toContain("section2");
+    expect(renderOnServer()).not.toContain("section3");
+    expect(renderOnServer()).not.toContain("Opt1");
+    expect(renderOnServer()).not.toContain("Opt2");
+    expect(renderOnServer()).not.toContain("Opt3");
+  });
+
+  it("can render RadioButtons", () => {
+    const renderOnServer = () =>
+      renderToString(
+        <RichSelectList {...defaultRequiredProps}>
+          <RichSelectList.RadioButton
+            label="Opt1"
+            value="opt1"
+            name="opt1name"
+          />
+          <RichSelectList.RadioButton
+            label="Opt2"
+            value="opt2"
+            name="opt2name"
+          />
+          <RichSelectList.RadioButton
+            label="Opt3"
+            value="opt3"
+            name="opt3name"
+          />
+        </RichSelectList>,
+      );
+    expect(renderOnServer).not.toThrow();
+    // popover is hidden, not in document
+    expect(renderOnServer()).not.toContain("Opt1");
+    expect(renderOnServer()).not.toContain("Opt2");
+    expect(renderOnServer()).not.toContain("Opt3");
+  });
+});
