5ceb281f9275106036ab5e4b371c95a7ef1dab1e
Christian Vuerings <christian@cambly.com>
2024-03-08 07:23:59 -0800

    Card: remove deprecated size prop (#328)

---
+++ .changeset/tasty-hornets-do.md
@@ -0,0 +1,5 @@
+---
+"@cambly/syntax-core": minor
+---
+
+Card: remove deprecated size prop
--- packages/syntax-core/src/Card/Card.test.tsx
+++ packages/syntax-core/src/Card/Card.test.tsx
@@ -16,36 +16,6 @@ describe("card", () => {
     expect(screen.getByText(/text/)).toBeInTheDocument();
   });
 
-  it("should have the right width for sm", () => {
-    render(
-      <Card size="sm" data-testid="card-sm">
-        <>
-          <h1>title</h1>
-          <p>text</p>
-        </>
-      </Card>,
-    );
-    expect(screen.getByTestId("card-sm")).toHaveStyle({
-      width: "100%",
-      maxWidth: "352px",
-    });
-  });
-
-  it("should have the right width for lg", () => {
-    render(
-      <Card size="lg" data-testid="card-lg">
-        <>
-          <h1>title</h1>
-          <p>text</p>
-        </>
-      </Card>,
-    );
-    expect(screen.getByTestId("card-lg")).toHaveStyle({
-      width: "100%",
-      maxWidth: "744px",
-    });
-  });
-
   it("should stretch to the width of the container", () => {
     render(
       <Box width={1000}>
