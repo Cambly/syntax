a2830a7fb5ee64cee61a1c9bfc05c56fa9b268cb
Christian Vuerings <christian@cambly.com>
2024-03-19 14:48:51 -0700

    Syntax cambio: export useTheme

--- packages/syntax-core/src/index.tsx
+++ packages/syntax-core/src/index.tsx
@@ -19,7 +19,7 @@ import SelectList from "./SelectList/SelectList";
 import TapArea from "./TapArea/TapArea";
 import TextArea from "./TextArea/TextArea";
 import TextField from "./TextField/TextField";
-import ThemeProvider from "./ThemeProvider/ThemeProvider";
+import ThemeProvider, { useTheme } from "./ThemeProvider/ThemeProvider";
 import Tooltip from "./Tooltip/Tooltip";
 import Typography from "./Typography/Typography";
 
@@ -48,4 +48,5 @@ export {
   ThemeProvider,
   Tooltip,
   Typography,
+  useTheme,
 };
