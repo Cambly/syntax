618a1b784fdefbd1f315c0a7fe6d5266b1e28867
Ben Orndorff <ben.orndorff@Ben-Orndorff-Mac-CVQHG6N14F.local>
2024-05-23 11:36:48 -0700

    initial

--- packages/syntax-core/src/Divider/Divider.tsx
+++ packages/syntax-core/src/Divider/Divider.tsx
@@ -1,3 +1,4 @@
+import { type ReactElement } from "react";
 import styles from "./Divider.module.css";
 import classNames from "classnames";
 import colorStyles from "../colors/colors.module.css";
@@ -5,15 +6,27 @@ import colorStyles from "../colors/colors.module.css";
 /**
  * [Divider](https://cambly-syntax.vercel.app/?path=/docs/components-divider--docs) is a thin horizontal line to group content in lists and layouts.
  */
-export default function Divider(): React.ReactElement {
+function Divider({
+  on = "lightBackground",
+}: {
+  /**
+   * Indicate whether the divider renders on a light or dark background. Changes the color of the divider.
+   *
+   * @defaulValue `lightBackground`
+   */
+  on?: "lightBackground" | "darkBackground";
+}): ReactElement {
   return (
     <hr
       className={classNames(
         styles.divider,
-        colorStyles.cambioGray370BackgroundColor,
+        {
+          [colorStyles.cambioGray370BackgroundColor]: on === "lightBackground",
+          [colorStyles.cambioWhite40BackgroundColor]: on === "darkBackground",
+        }
       )}
     />
   );
 }
 
-Divider.displayName = "Divider";
+export default Divider;
