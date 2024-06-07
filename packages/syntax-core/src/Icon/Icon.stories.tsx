b0bdf62ef79169d215eb5aa9eed781caa2db713d
Joe Liotta <joe.liotta00@gmail.com>
2024-06-07 13:00:36 -0700

    Icon: Update sizing to match Typography scale (#443)
    * Update sizes
    
    * Update stories
    
    * Add changeset
    
    * Update icon size consts

--- packages/syntax-core/src/Icon/Icon.stories.tsx
+++ packages/syntax-core/src/Icon/Icon.stories.tsx
@@ -9,7 +9,7 @@ export default {
   },
   argTypes: {
     size: {
-      options: [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
+      options: [0, 100, 200, 300, 400, 500, 600, 700, 800, 900],
       control: { type: "radio" },
     },
   },
