e0e270156f11ecc15cda3f0c458f7c355ca3eac7
Farzan <farzan.hashmi@cambly.com>
2024-07-16 23:22:41 -0700

    tests

--- packages/syntax-core/src/IconLinkButton/IconLinkButton.stories.tsx
+++ packages/syntax-core/src/IconLinkButton/IconLinkButton.stories.tsx
@@ -17,6 +17,7 @@ export default {
     size: "md",
     "data-testid": "",
     accessibilityLabel: "",
+    loading: false,
   },
   argTypes: {
     color: {
--- packages/syntax-core/src/IconLinkButton/IconLinkButton.test.tsx
+++ packages/syntax-core/src/IconLinkButton/IconLinkButton.test.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { screen, render } from "@testing-library/react";
 import IconLinkButton from "./IconLinkButton";
 import StarFilled from "../../../syntax-icons/src/icons/StarFilled";
+import { vi } from "vitest";
+import userEvent from "@testing-library/user-event";
 
 describe("iconLinkButton", () => {
   it("renders successfully", () => {
@@ -25,5 +27,75 @@ describe("iconLinkButton", () => {
     );
     const button = await screen.findAllByLabelText("Continue to the next step");
     expect(button).toHaveLength(1);
+  });
+
+  it("fires the onClick when clicked", async () => {
+    const handleClick = vi.fn();
+    render(
+      <IconLinkButton
+        onClick={handleClick}
+        icon={StarFilled}
+        href="https://www.google.com"
+        accessibilityLabel="Continue to the next step"
+      />,
+    );
+    const button = await screen.findAllByLabelText("Continue to the next step");
+    await userEvent.click(button[0]);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  it("sets the data-testid", () => {
+    render(
+      <IconLinkButton
+        icon={StarFilled}
+        href="https://www.google.com"
+        accessibilityLabel="Continue to the next step"
+        data-testid="test-id"
+      />,
+    );
+    expect(screen.getByTestId("test-id")).toBeInTheDocument();
+  });
+  it("has the correct target", async () => {
+    render(
+      <IconLinkButton
+        href="https://www.google.com"
+        target="_blank"
+        data-testid="iconlinkbutton-test-id"
+        accessibilityLabel="Continue to the next step"
+        icon={StarFilled}
+      />,
+    );
+    const linkButton = await screen.findByTestId("iconlinkbutton-test-id");
+    expect(linkButton).toHaveAttribute("target", "_blank");
+  });
+
+  it("has the correct href", async () => {
+    render(
+      <IconLinkButton
+        href="https://www.google.com"
+        data-testid="iconlinkbutton-test-id"
+        accessibilityLabel="Continue to the next step"
+        icon={StarFilled}
+      />,
+    );
+    const linkButton = await screen.findByTestId("iconlinkbutton-test-id");
+    expect(linkButton).toHaveAttribute("href", "https://www.google.com");
+  });
+
+  it("allows ref to be set", () => {
+    const ref = React.createRef<HTMLAnchorElement>();
+    render(
+      <IconLinkButton
+        href="https://www.google.com"
+        ref={ref}
+        accessibilityLabel="Continue to the next step"
+        icon={StarFilled}
+        data-testid="iconlinkbutton-test-id"
+      />,
+    );
+    expect(ref.current instanceof HTMLAnchorElement).toBeTruthy();
+    expect(ref.current?.getAttribute("data-testid")).toStrictEqual(
+      "iconlinkbutton-test-id",
+    );
   });
 });
