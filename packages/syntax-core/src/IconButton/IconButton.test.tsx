e6650cca3d930b80db81d9c523becb2e194dcedd
William Newman <william@cambly.com>
2024-08-13 16:40:11 -0400

    hopefully this works

--- packages/syntax-core/src/IconButton/IconButton.test.tsx
+++ packages/syntax-core/src/IconButton/IconButton.test.tsx
@@ -74,7 +74,7 @@ describe("iconButton", () => {
     expect(button).toHaveLength(1);
   });
 
-  it("fires the onClick when it is clicked", async () => {
+  it("fires the onClick when clicked", async () => {
     const handleClick = vi.fn();
     render(
       <IconButton
