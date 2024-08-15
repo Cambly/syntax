4bb4cd03b001756cdc492a8f00ec390598cfcf64
Wilson Yu <hello@wilsonyu.io>
2024-08-15 08:48:08 -0700

    update icons

--- packages/syntax-icons/src/icons/Clock.tsx
+++ packages/syntax-icons/src/icons/Clock.tsx
@@ -5,7 +5,7 @@ const Clock = forwardRef<
   Omit<ComponentProps<typeof Icon>, "path">
 >(({ color, size }, ref) => {
   const path =
-    "M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2m-1 2h2v7.6h-2zm5.3 13.7L11.6 13l1.4-1.4 4.7 4.7z";
+    "M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2m-1 2h2v7.6l-2 .79zm5.3 13.7L11 12.39l2-.79 4.7 4.7z";
   return <Icon ref={ref} path={path} color={color} size={size} />;
 });
 Clock.displayName = "Clock";
