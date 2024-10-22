22e0e8008fb90b236d4f619250f180cc618a2506
Wilson Yu <hello@wilsonyu.io>
2024-10-22 01:54:56 -0700

    update index.tsx

--- packages/syntax-floating-components/src/index.tsx
+++ packages/syntax-floating-components/src/index.tsx
@@ -1,11 +1,4 @@
-import { Tooltip, TooltipContent, TooltipTrigger } from "./Tooltip/Tooltip";
+import { Tooltip } from "./Tooltip/Tooltip";
 import { Popover, PopoverContent, PopoverTrigger } from "./Popover/Popover";
 
-export {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-};
+export { Popover, PopoverContent, PopoverTrigger, Tooltip };
