88936c7f639f1d45e4509fa299347c0dc4cdccb5
Wilson Yu <hello@wilsonyu.io>
2024-10-22 01:59:21 -0700

    update tests

--- packages/syntax-floating-components/src/Tooltip/Tooltip.test.tsx
+++ packages/syntax-floating-components/src/Tooltip/Tooltip.test.tsx
@@ -1,5 +1,5 @@
 import { screen, render } from "@testing-library/react";
-import { Tooltip, TooltipContent, TooltipTrigger } from "./Tooltip";
+import { Tooltip } from "./Tooltip";
 import { expect } from "vitest";
 import userEvent from "@testing-library/user-event";
 class ResizeObserver {
@@ -19,11 +19,8 @@ window.ResizeObserver = ResizeObserver;
 describe("tooltip", () => {
   it("renders successfully", () => {
     render(
-      <Tooltip>
-        <TooltipTrigger>
-          <button>My trigger</button>
-        </TooltipTrigger>
-        <TooltipContent>My tooltip</TooltipContent>
+      <Tooltip content="My tooltip">
+        <button>My trigger</button>
       </Tooltip>,
     );
     expect(screen.getByRole("button")).toBeInTheDocument();
@@ -32,11 +29,8 @@ describe("tooltip", () => {
 
   it("tooltip dialogue appears", async () => {
     render(
-      <Tooltip>
-        <TooltipTrigger>
-          <button>My trigger</button>
-        </TooltipTrigger>
-        <TooltipContent>My tooltip</TooltipContent>
+      <Tooltip content="My tooltip">
+        <button>My trigger</button>
       </Tooltip>,
     );
 
