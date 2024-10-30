3505c43e54d6fd0fb58ddd1d9b961a4195f5e340
Chia-Chi Hsu <chessy2206@gmail.com>
2024-10-30 15:35:15 -0700

    same args and argTypes for TapArea

---
+++ packages/syntax-core/src/LinkTapArea/LinkTapArea.stories.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { type StoryObj, type Meta } from "@storybook/react";
+import LinkTapArea from "./LinkTapArea";
+import Box from "../Box/Box";
+import Typography from "../Typography/Typography";
+import Avatar from "../Avatar/Avatar";
+import image from "../../../../apps/storybook/assets/images/jane.webp";
+
+export default {
+  title: "Components/LinkTapArea",
+  component: LinkTapArea,
+  args: {
+    disabled: false,
+    rounding: "none",
+    tabIndex: 0,
+    fullWidth: true,
+    accessibilityLabel: "",
+    "data-testid": "",
+  },
+  argTypes: {
+    disabled: {
+      control: "boolean",
+    },
+    onClick: { action: "clicked" },
+    rounding: {
+      options: ["none", "sm", "md", "lg", "xl", "full"],
+      control: { type: "select" },
+    },
+    tabIndex: {
+      control: { type: "number" },
+      options: [0, -1],
+    },
+  },
+  tags: ["autodocs"],
+} as Meta<typeof LinkTapArea>;
+
+export const Default: StoryObj<typeof LinkTapArea> = {
+  args: {
+    children: (
+      <Box display="flex" alignItems="center" gap={4} padding={2}>
+        <Avatar accessibilityLabel="Jane" src={image} size="md" />
+        <Typography size={400}>Jane Doe</Typography>
+      </Box>
+    ),
+  },
+  render: ({ ...args }) => <LinkTapArea {...args} />,
+};
