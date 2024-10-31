796c9bc77bb6461accc594731ecaa539b1caf7be
Chia-Chi Hsu <chessy2206@gmail.com>
2024-10-31 15:06:32 -0700

    tests for a tag

--- packages/syntax-core/src/LinkTapArea/LinkTapArea.test.tsx
+++ packages/syntax-core/src/LinkTapArea/LinkTapArea.test.tsx
@@ -30,6 +30,56 @@ describe("linkTapArea", () => {
     expect(child).toHaveTextContent("Continue");
   });
 
+  it("has the correct target", async () => {
+    render(
+      <LinkTapArea
+        href="https://www.google.com"
+        target="_blank"
+        data-testid="link-tap-area-testid"
+        onClick={() => {
+          /* empty */
+        }}
+      />,
+    );
+    const linkTapArea = await screen.findByTestId("link-tap-area-testid");
+    expect(linkTapArea).toHaveAttribute("target", "_blank");
+  });
+
+  it("has the correct href", async () => {
+    render(
+      <LinkTapArea
+        href="https://www.google.com"
+        target="_blank"
+        data-testid="link-tap-area-testid"
+        onClick={() => {
+          /* empty */
+        }}
+      />,
+    );
+    const linkTapArea = await screen.findByTestId("link-tap-area-testid");
+    expect(linkTapArea).toHaveAttribute("href", "https://www.google.com");
+  });
+
+  it("allows ref to be set", () => {
+    const ref = createRef<HTMLAnchorElement>();
+
+    render(
+      <LinkTapArea
+        href="https://www.google.com"
+        target="_blank"
+        data-testid="link-tap-area-testid"
+        onClick={() => {
+          /* empty */
+        }}
+        ref={ref}
+      />,
+    );
+    expect(ref.current instanceof HTMLAnchorElement).toBeTruthy();
+    expect(ref.current?.getAttribute("data-testid")).toStrictEqual(
+      "link-tap-area-testid",
+    );
+  });
+
   it("fires onClick when clicked and the LinkTapArea is enabled", async () => {
     const handleTap = vi.fn();
     render(
