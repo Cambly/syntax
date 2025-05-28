9410b0ff0f4cc8482203ccbd5f283523f082a1c6
Srini Panguluri <srini91@yahoo.com>
2025-05-28 12:07:45 -0700

    design update from Heather

--- packages/syntax-icons/src/icons/NewTab.tsx
+++ packages/syntax-icons/src/icons/NewTab.tsx
@@ -5,7 +5,7 @@ const NewTab = forwardRef<
   Omit<ComponentProps<typeof Icon>, "path">
 >(({ color, size }, ref) => {
   const path =
-    "M.544.536V23.53h23v-8.586h-2.751v5.821H3.295V3.287h5.83V.529H.544Zm13.538 0V3.28h5.015l-9.753 9.743 1.95 1.943 9.499-9.495v4.518h2.751V.536Z";
+    "M2 2v20h20v-7.464h-2.39v5.07H4.393V4.39h5.07V2Zm11.771 0v2.39h4.36l-8.474 8.475 1.69 1.691 8.262-8.258v3.93H22V2Z";
   return <Icon ref={ref} path={path} color={color} size={size} />;
 });
 NewTab.displayName = "NewTab";
