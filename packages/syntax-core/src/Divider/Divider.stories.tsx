be79f2796c9b7bf23011556199a559a00285325a
Philip Larson <philip@cambly.com>
2026-02-23 15:09:14 -0800

    Add optional `color` prop to Divider component. (#650)
    * Add optional `color` prop to Divider component.
    
    Accepts "white40", "white70", "gray270", "gray370", "gray870", "cream", "lilac", "navy", "orange", "pink", "purple", "red", "sky", "slate", "tan", "teal", or "thistle".
    Defaults to "gray370".
    
    * Cleanup docs for better storybook appearance.

--- packages/syntax-core/src/Divider/Divider.stories.tsx
+++ packages/syntax-core/src/Divider/Divider.stories.tsx
@@ -11,6 +11,33 @@ export default {
       url: "https://www.figma.com/file/p7LKna9JMU0JEkcKamzs53/Cambly-Design-System?node-id=1007%3A4101",
     },
   },
+  args: {
+    color: "gray370",
+  },
+  argTypes: {
+    color: {
+      options: [
+        "white40",
+        "white70",
+        "gray270",
+        "gray370",
+        "gray870",
+        "cream",
+        "lilac",
+        "navy",
+        "orange",
+        "pink",
+        "purple",
+        "red",
+        "sky",
+        "slate",
+        "tan",
+        "teal",
+        "thistle",
+      ],
+      control: { type: "radio" },
+    },
+  },
   tags: ["autodocs"],
 } as Meta<typeof Divider>;
 
--- packages/syntax-core/src/Divider/Divider.tsx
+++ packages/syntax-core/src/Divider/Divider.tsx
@@ -2,15 +2,46 @@ import styles from "./Divider.module.css";
 import classNames from "classnames";
 import colorStyles from "../colors/colors.module.css";
 
+const dividerColors = [
+  "white40",
+  "white70",
+  "gray270",
+  "gray370",
+  "gray870",
+  "cream",
+  "lilac",
+  "navy",
+  "orange",
+  "pink",
+  "purple",
+  "red",
+  "sky",
+  "slate",
+  "tan",
+  "teal",
+  "thistle",
+] as const;
+
+type DividerColors = (typeof dividerColors)[number];
+
 /**
  * [Divider](https://cambly-syntax.vercel.app/?path=/docs/components-divider--docs) is a thin horizontal line to group content in lists and layouts.
  */
-export default function Divider(): React.ReactElement {
+export default function Divider({
+  color = "gray370",
+}: {
+  /**
+   * The color of the divider.
+   *
+   * @defaultValue "gray370"
+   */
+  color?: DividerColors;
+}): React.ReactElement {
   return (
     <hr
       className={classNames(
         styles.divider,
-        colorStyles.cambioGray370BackgroundColor,
+        colorStyles[`${color}BackgroundColor`],
       )}
     />
   );
